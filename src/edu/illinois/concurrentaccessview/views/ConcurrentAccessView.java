85202e0c5d1d00c9c0eff1c87845edeadb99ca02
Anda Bereczky <anda@bereczky.me>
2011-12-18 19:52:55 -0600

    Moved the analysis into the Parallel Array refactoring plug-in.

--- src/edu/illinois/concurrentaccessview/views/ConcurrentAccessView.java
+++ src/edu/illinois/concurrentaccessview/views/ConcurrentAccessView.java
@@ -5,20 +5,12 @@ import java.util.Map;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.jdt.core.IClasspathEntry;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.IPackageFragmentRoot;
-import org.eclipse.jdt.core.JavaModelException;
-import org.eclipse.jdt.internal.core.CompilationUnit;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
@@ -26,22 +18,19 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE;
 import org.eclipse.ui.part.ViewPart;
 
-import sabazios.domains.ConcurrentAccesses;
 import sabazios.domains.Loop;
 import sabazios.domains.ObjectAccess;
 import sabazios.util.CodeLocation;
-import edu.illinois.concurrentaccessview.analysis.DataRaceAnalysis;
 
 /**
  * Main class for the concurrent access view, which is used to display
  * concurrent accesses in code.
  */
-public class ConcurrentAccessView extends ViewPart implements ISelectionListener {
+public class ConcurrentAccessView extends ViewPart {
 
 	/**
 	 * The ID of the view as specified by the extension.
@@ -96,6 +85,10 @@ public class ConcurrentAccessView extends ViewPart implements ISelectionListener
 		Label traceLabel = new Label(parent, SWT.LEFT);
 	}
 
+	public IFile getFile() {
+		return file;
+	}
+
 	/**
 	 * Hooks a double-click listener to the tree viewer.
 	 */
@@ -142,7 +135,7 @@ public class ConcurrentAccessView extends ViewPart implements ISelectionListener
 			}
 		};
 	}
-	
+
 	/**
 	 * Open the selected file in the Java editor and put a marker at the given
 	 * line number.
@@ -154,82 +147,29 @@ public class ConcurrentAccessView extends ViewPart implements ISelectionListener
 		HashMap<String, Object> markerAttributes = new HashMap<String, Object>();
 		markerAttributes.put(IMarker.LINE_NUMBER, new Integer(line));
 		markerAttributes.put(IDE.EDITOR_ID_ATTR, "org.eclipse.jdt.ui.CompilationUnitEditor");
-		IMarker marker = this.file.createMarker(IMarker.TEXT);
+		IMarker marker = this.getFile().createMarker(IMarker.TEXT);
 		marker.setAttributes(markerAttributes);
 		IDE.openEditor(activePage, marker);
 		marker.delete();
 	}
-
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.ISelectionListener#selectionChanged(org.eclipse.ui.IWorkbenchPart, org.eclipse.jface.viewers.ISelection)
-	 */
-	@Override
-	public void selectionChanged(IWorkbenchPart part, ISelection selection) {
-		if (selection instanceof TreeSelection) {
-			if (((TreeSelection) selection).getFirstElement() != null) {
-				Object firstSelectedElement = ((TreeSelection) selection).getFirstElement();
-				if (firstSelectedElement instanceof CompilationUnit) {
-					try {
-						
-						CompilationUnit cu = (CompilationUnit) firstSelectedElement;
-						
-						DataRaceAnalysis analysis = new DataRaceAnalysis();
-						
-						String className = cu.getElementName();
-						if (className.endsWith(".java")) {
-							className = className.substring(0, className.length() - 5);
-						}
-						
-						IJavaProject javaProject = cu.getJavaProject();
-						IPath absoluteWorkspacePath = javaProject.getResource().getLocation(); // E:/eclipse/runtime-EclipseApplication/Test
-						if (absoluteWorkspacePath.lastSegment().equals(javaProject.getElementName())) {
-							absoluteWorkspacePath = absoluteWorkspacePath.removeLastSegments(1);
-							absoluteWorkspacePath = absoluteWorkspacePath.removeTrailingSeparator();
-						} // e:/eclipse/runtime-EclipseApplication
-						
-						IPath relativeBinPath = javaProject.getOutputLocation(); // /Test/bin
-						IPath absoluteBinPath = absoluteWorkspacePath.append(relativeBinPath); // E:/eclipse/runtime-EclipseApplication/Test/bin
-						analysis.addBinaryDependency(absoluteBinPath.toString());
-						
-						IClasspathEntry[] rawClasspath = javaProject.getRawClasspath();
-						for (int i = 0; i < rawClasspath.length; i++) {
-							IClasspathEntry classpathEntry = rawClasspath[i];
-							if (javaProject.getPath().isPrefixOf(classpathEntry.getPath()) &&
-									classpathEntry.getContentKind() == IPackageFragmentRoot.K_BINARY &&
-									classpathEntry.getEntryKind() == IClasspathEntry.CPE_LIBRARY) {
-								IPath absoluteLibraryPath = absoluteWorkspacePath.append(classpathEntry.getPath());
-								analysis.addJarDependency(absoluteLibraryPath.toString());
-							}
-						}
-						
-						StringBuffer entryClass = new StringBuffer();
-						entryClass.append('L');
-						char[][] entryPackage = cu.getPackageName();
-						for (int i = 0; i < entryPackage.length; i++) {
-							entryClass.append(entryPackage[i]);
-							entryClass.append('/');
-						}
-						entryClass.append(className);
-						
-						ConcurrentAccesses<?> ca = analysis.findConcurrentAccesses(entryClass.toString(), "verySimpleRace()V");
-						this.viewer.setInput(ca);
-						
-						IPath p = cu.getPath();
-						IProject project = javaProject.getProject();
-						this.file = project.getFile(p.removeFirstSegments(1));
-					} catch (JavaModelException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}
+	
+	public void setFile(IFile file) {
+		this.file = file;
 	}
-
+	
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.part.WorkbenchPart#setFocus()
 	 */
 	@Override
 	public void setFocus() {
 		viewer.getControl().setFocus();
+	}
+
+	/**
+	 * Sets the given object as the input of the tree viewer.
+	 * @param input
+	 */
+	public void setInput(Object input) {
+		this.viewer.setInput(input);
 	}
 }
