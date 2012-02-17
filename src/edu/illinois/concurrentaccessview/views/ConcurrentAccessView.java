55affb85f4d00d5b28ecd434303386434d609b50
unknown <Anda@Anda-PC.(none)>
2012-02-16 18:51:33 -0600

    Multiple changes: - implemented console with hyperlinks - implemented line warnings - made it unnecessary to select function names to run the ReLooper analysis. - made view columns resizable

--- src/edu/illinois/concurrentaccessview/views/ConcurrentAccessView.java
+++ src/edu/illinois/concurrentaccessview/views/ConcurrentAccessView.java
@@ -2,26 +2,34 @@ package edu.illinois.concurrentaccessview.views;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.internal.debug.ui.console.JavaStackTraceConsole;
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.TextConsoleViewer;
 import org.eclipse.ui.ide.IDE;
 import org.eclipse.ui.part.ViewPart;
 
+import sabazios.domains.ConcurrentAccess;
+import sabazios.domains.ConcurrentAccesses;
 import sabazios.domains.Loop;
 import sabazios.domains.ObjectAccess;
 import sabazios.util.CodeLocation;
@@ -43,9 +51,9 @@ public class ConcurrentAccessView extends ViewPart {
 	private TreeViewer viewer;
 	
 	/**
-	 * A label used to display the trace of a concurrent access.
+	 * A console used to display the trace of a concurrent access.
 	 */
-	private Label traceLabel;
+	private JavaStackTraceConsole console;
 
 	/**
 	 * Run when the user double-clicks an item in the tree viewer.
@@ -56,6 +64,65 @@ public class ConcurrentAccessView extends ViewPart {
 	 * The file in which concurrent accesses are being analyzed.
 	 */
 	private IFile file;
+	
+	/**
+	 * A map of code locations to warning markers.
+	 */
+	private Map<CodeLocation, IMarker> markers = new HashMap<CodeLocation, IMarker>();
+
+	/**
+	 * Clear all the markers in the current file.
+	 */
+	private void clearMarkers() {
+		for (IMarker marker : this.markers.values()) {
+			try {
+				marker.delete();
+			} catch (CoreException e) {
+				// do nothing
+			}
+		}
+		this.markers.clear();
+	}
+
+	/**
+	 * Create a warning marker from the given concurrent access.
+	 * @param oa A concurrent access object
+	 * @throws CoreException
+	 */
+	private void createMarker(ObjectAccess oa) throws CoreException {
+		CodeLocation cl = oa.getCodeLocation();
+		IMarker marker = this.getFile().createMarker(IMarker.PROBLEM);
+		marker.setAttributes(
+				new String[] {IMarker.MESSAGE, IMarker.SEVERITY, IMarker.LINE_NUMBER, IDE.EDITOR_ID_ATTR},
+				new Object[] {"Concurrent access detected: " + oa.toString(), IMarker.SEVERITY_WARNING, new Integer(cl.getLineNo()), "org.eclipse.jdt.ui.CompilationUnitEditor"});
+		this.markers.put(cl, marker);
+	}
+
+	/**
+	 * Create warning markers from the given concurrent accesses.
+	 * @param cas A concurrent accesses object
+	 */
+	private void createMarkers(ConcurrentAccesses cas) {
+		for (Object obj : cas.values()) {
+			if (obj instanceof Set) {
+				for (Object obj2 : (Set) obj) {
+					if (obj2 instanceof ConcurrentAccess) {
+						ConcurrentAccess ca = (ConcurrentAccess) obj2;
+						for (Object obj3 : ca.alphaAccesses) {
+							if (obj3 instanceof ObjectAccess) {
+								ObjectAccess oa = (ObjectAccess) obj3;
+								try {
+									this.createMarker(oa);
+								} catch (CoreException e) {
+									// do nothing
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.part.WorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
@@ -63,13 +130,18 @@ public class ConcurrentAccessView extends ViewPart {
 	@Override
 	public void createPartControl(Composite parent) {
 		
-		// Create a layout with two equal-width columns.
-		FillLayout fillLayout = new FillLayout(SWT.HORIZONTAL);
-		fillLayout.spacing = 4;
-		parent.setLayout(fillLayout);
+		// Set the layout of the parent control.
+		parent.setLayout(new FillLayout());
+		
+		// Create a sash form with two equal-width columns.
+		SashForm form = new SashForm(parent, SWT.HORIZONTAL);
+		form.setLayout(new FillLayout());
+		form.setWeights(new int[] {50, 50});
 		
 		// Create the tree viewer.
-		viewer = new TreeViewer(parent);
+		Composite child1 = new Composite(form, SWT.NONE);
+		child1.setLayout(new FillLayout());
+		viewer = new TreeViewer(child1, SWT.NONE);
 		viewer.setContentProvider(new ViewContentProvider());
 		viewer.setLabelProvider(new ViewLabelProvider());
 		viewer.setSorter(new NameSorter());
@@ -79,8 +151,13 @@ public class ConcurrentAccessView extends ViewPart {
 		makeActions();
 		hookDoubleClickAction();
 		
-		// Create the trace label.
-		Label traceLabel = new Label(parent, SWT.LEFT);
+		// Create the trace console.
+		console = new JavaStackTraceConsole();
+        IConsoleManager consoleManager = ConsolePlugin.getDefault().getConsoleManager();
+		consoleManager.addConsoles(new IConsole[]{console});
+		Composite child2 = new Composite(form, SWT.NONE);
+		child2.setLayout(new FillLayout());
+		new TextConsoleViewer(child2, console);
 	}
 
 	public IFile getFile() {
@@ -97,7 +174,7 @@ public class ConcurrentAccessView extends ViewPart {
 			}
 		});
 	}
-
+	
 	/**
 	 * Creates all actions needed by this view.
 	 */
@@ -110,25 +187,22 @@ public class ConcurrentAccessView extends ViewPart {
 				ISelection selection = viewer.getSelection();
 				Object obj = ((IStructuredSelection) selection).getFirstElement();
 				
-				CodeLocation cl = null;
-				
-				if (obj instanceof Map.Entry) {
-					Object obj2 = ((Map.Entry) obj).getKey();
-					if (obj2 instanceof Loop) {
-						Loop loop = (Loop) obj2;
-						cl = loop.getCodeLocation();
-					}
-				} else if (obj instanceof ObjectAccess) {
-					ObjectAccess oa = (ObjectAccess) obj;
-					cl = oa.getCodeLocation();
-				}
+				IDocument doc = console.getDocument();
+				doc.set(obj.toString());
 				
 				try {
-					if (cl != null) {
-						openFileInEditor(cl.getLineNo());
+					if (obj instanceof Map.Entry) {
+						Object obj2 = ((Map.Entry) obj).getKey();
+						if (obj2 instanceof Loop) {
+							Loop loop = (Loop) obj2;
+							openCodeLocation(loop.getCodeLocation());
+						}
+					} else if (obj instanceof ObjectAccess) {
+						ObjectAccess oa = (ObjectAccess) obj;
+						openCodeLocation(oa.getCodeLocation());
 					}
 				} catch (CoreException e) {
-					e.printStackTrace();
+					// do nothing
 				}
 			}
 		};
@@ -136,25 +210,31 @@ public class ConcurrentAccessView extends ViewPart {
 
 	/**
 	 * Open the selected file in the Java editor and put a marker at the given
-	 * line number.
-	 * @param line A line number in the currently selected file.
+	 * code location.
+	 * @param codeLocation A code location in the currently selected file.
 	 * @throws CoreException
 	 */
-	private void openFileInEditor(int line) throws CoreException {
+	private void openCodeLocation(CodeLocation codeLocation) throws CoreException {
+		
 		IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-		HashMap<String, Object> markerAttributes = new HashMap<String, Object>();
-		markerAttributes.put(IMarker.LINE_NUMBER, new Integer(line));
-		markerAttributes.put(IDE.EDITOR_ID_ATTR, "org.eclipse.jdt.ui.CompilationUnitEditor");
-		IMarker marker = this.getFile().createMarker(IMarker.TEXT);
-		marker.setAttributes(markerAttributes);
+		IMarker marker;
+		
+		if (this.markers.containsKey(codeLocation)) {
+			marker = this.markers.get(codeLocation);
+		} else {
+			marker = this.getFile().createMarker(IMarker.TEXT);
+			marker.setAttributes(
+					new String[] {IMarker.LINE_NUMBER, IDE.EDITOR_ID_ATTR},
+					new Object[] {new Integer(codeLocation.getLineNo()), "org.eclipse.jdt.ui.CompilationUnitEditor"});
+		}
+		
 		IDE.openEditor(activePage, marker);
-		marker.delete();
 	}
 	
 	public void setFile(IFile file) {
 		this.file = file;
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.part.WorkbenchPart#setFocus()
 	 */
@@ -169,5 +249,9 @@ public class ConcurrentAccessView extends ViewPart {
 	 */
 	public void setInput(Object input) {
 		this.viewer.setInput(input);
+		if (input instanceof ConcurrentAccesses) {
+			this.clearMarkers();
+			this.createMarkers((ConcurrentAccesses) input);
+		}
 	}
 }
